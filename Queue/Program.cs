b8ed6cc1675255b3dc2c3ddccae3ab9d358064b6
sakthidineshSF4937 <sakthidinesh.pownraj@syncfusion.com>
2024-12-21 02:06:28 +0530

    Queue

---
+++ Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Queue;
+
+public class Program{
+    public static void Main(string[] args)
+    {
+        //creating the queue
+       CustomQueue<int> customQueue=new CustomQueue<int>();
+       //Adding Elements to the queue
+       customQueue.EnQueue(1);
+       customQueue.EnQueue(2);
+       customQueue.EnQueue(3);
+       customQueue.EnQueue(4);
+
+       customQueue.EnQueue(5);
+
+        //Iterating to the object
+       foreach(int i  in customQueue ){
+        System.Console.Write(i + " ");
+       }
+       //peeking the first Element in Queue
+       System.Console.WriteLine(customQueue.Peek());
+       customQueue.EnQueue(5);
+       //Reoving the first Element and diplaying
+       customQueue.Dequeue();
+    }
+}
